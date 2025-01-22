b50d7b8b4b2daa55e698ee3017b04b5c985b9562
theskillboost1 <theskillboost.dm@gmail.com>
2025-01-22 10:49:35 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -8,31 +8,30 @@ const Path = require('path');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(express.static(Path.join(__dirname,`../dream`)))
+app.use(express.static(Path.join(__dirname, `../dream`)))
 
 app.use(cors());
+
 mongoose.connect('mongodb+srv://sawalarora20:Supersawal@cluster0.dtd4w.mongodb.net/theskillboost?retryWrites=true&w=majority')
     .then(() => console.log("connected"))
     .catch((err) => console.log(err));
 
 const UserSchema = new mongoose.Schema({
     Name: String,
-    Date: String,
     Email: String,
     Contact: String,
     Qualification: String,
-    Date: String,
     Course: String
-
-
 
 });
 
 const UserModel = mongoose.model("user", UserSchema);
 
-// app.get('/', (req, res) => {
-//     res.sendFile(Path.join(__dirname, '../dream/index.html'));
-// });
+app.get('/', (req, res) => {
+    res.sendFile(Path.join(__dirname, '../dream'));
+});
+app.use(express.static('../dream')); 
+
 
 
 // app.get('/admin', (req, res) => {
@@ -50,12 +49,12 @@ app.get('/dashboard', (req, res) => {
 
 
 app.post('/create', (req, res) => {
-    
-    const { Name, Email, Contact,Qualification,Course,Date } = req.body;
 
-    UserModel.create({ Name, Email, Contact,Qualification,Course,Date })
+    const { Name, Email, Contact, Qualification, Course } = req.body;
+
+    UserModel.create({ Name, Email, Contact, Qualification, Course })
         .then((data) => {
-            res.json(data);  
+            res.json(data);
         })
         .catch((err) => {
             console.error(err);
@@ -63,13 +62,11 @@ app.post('/create', (req, res) => {
         });
 });
 
-
-
 app.delete('/delete/:id', (req, res) => {
     const id = req.params.id;
     console.log("Deleting user with ID:", id);
 
-    
+
     if (!mongoose.Types.ObjectId.isValid(id)) {
         return res.status(400).json({ error: 'Invalid ObjectId' });
     }
@@ -79,7 +76,7 @@ app.delete('/delete/:id', (req, res) => {
             if (!response) {
                 return res.status(404).json({ error: 'User not found' });
             }
-           
+
             res.json({ message: 'User deleted successfully', user: response });
         })
         .catch(err => {
@@ -91,12 +88,10 @@ app.delete('/delete/:id', (req, res) => {
 
 const UserSchema1 = new mongoose.Schema({
     Name: String,
-    Date: String,
     email: String,
     Number: String,
-    Subject: String,
     Message: String,
-  
+    Subject: String
 
 
 
@@ -117,13 +112,12 @@ app.get('/dashboard1', (req, res) => {
 
 
 app.post('/create1', (req, res) => {
-    
-    const { Name, Date, email, Number,Message,Subject, } = req.body;
 
-    UserModel1.create({ Name, Date, email, Number,Message,Subject, })
+    const { Name, email, Number, Message, Subject } = req.body;
+
+    UserModel1.create({ Name, email, Number, Message, Subject })
         .then((data) => {
-            res.json(data);  
-            res.redirect('/');
+            res.json(data);
         })
         .catch((err) => {
             console.error(err);
@@ -137,7 +131,7 @@ app.delete('/delete1/:id', (req, res) => {
     const id = req.params.id;
     console.log("Deleting user with ID:", id);
 
-    
+
     if (!mongoose.Types.ObjectId.isValid(id)) {
         return res.status(400).json({ error: 'Invalid ObjectId' });
     }
@@ -147,7 +141,7 @@ app.delete('/delete1/:id', (req, res) => {
             if (!response) {
                 return res.status(404).json({ error: 'User not found' });
             }
-           
+
             res.json({ message: 'User deleted successfully', user: response });
         })
         .catch(err => {
@@ -155,6 +149,53 @@ app.delete('/delete1/:id', (req, res) => {
             res.status(500).json({ error: 'An error occurred while deleting the user' });
         });
 });
+const BlogSchema = new mongoose.Schema({
+    Title: String,
+    Description: String,
+    Date: String,
+    Description1: String,
+    Slug: { type: String, unique: true }  // Slug field for unique URLs
+});
 
 
-app.listen(8082);
+
+const BlogModel = mongoose.model("Blogs", BlogSchema);
+
+app.post("/createblog", (req, res) => {
+    const { Title, Description, Date, Description1,Slug } = req.body;
+    BlogModel.create({ Title, Description,Description1, Date,Slug })
+        .then((blog) => {
+            res.status(201).json(blog);  
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).json({ message: "Error creating blog", error: err });
+        });
+});
+
+// Fetch all blogs
+app.get("/show", (req, res) => {
+    BlogModel.find({})
+        .then((blogs) => {
+            res.json(blogs);  // Return all blogs
+        })
+        .catch((err) => res.json(err));
+});
+
+
+app.get("/blog/:slug", (req, res) => {
+    const { slug } = req.params;
+    BlogModel.findOne({ Slug: slug })
+        .then((blog) => {
+            if (blog) {
+                res.json(blog);  // Return the blog as JSON
+            } else {
+                res.status(404).json({ message: "Blog not found" });
+            }
+        })
+        .catch((err) => res.status(500).json({ message: "Error fetching blog", error: err }));
+});
+
+
+
+app.listen(8081);
